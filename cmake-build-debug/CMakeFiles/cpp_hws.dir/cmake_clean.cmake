6cb5c26e223564e5c2461182fa2889ef8ffc0ddb
Michael Shevelin <shevel55505@gmail.com>
2020-11-07 21:38:07 +0200

    adding files

---
+++ cmake-build-debug/CMakeFiles/cpp_hws.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_hws.dir/Calculator.cpp.o"
+  "CMakeFiles/cpp_hws.dir/Parser.cpp.o"
+  "CMakeFiles/cpp_hws.dir/Set.cpp.o"
+  "CMakeFiles/cpp_hws.dir/main.cpp.o"
+  "cpp_hws"
+  "cpp_hws.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_hws.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
